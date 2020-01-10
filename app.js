b9f731c857cb38da300b4be88db9e8e5258ee305
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-01-10 16:16:59 +0300

    Alpha version business logik

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ class User {
 		this.rightAnswers = [];
 	}
 	getFullName() {
-		return `Player ${this.firstName} ${this.lastName}`;
+		return `Player: ${this.firstName} ${this.lastName}`;
 	}
 	setLevel(level) {
-		this.level = level;
+		if (level === 'EASY') {
+			this.level = 1;
+		} else if (level === 'MEDIUM') {
+			this.level = 2;
+		} else if (level === 'HARD') {
+			this.level = 3;
+		}
+	}
+	getLevel() {
+		return this.level;
 	}
 }
 
@@ -100,23 +109,26 @@ const taskArr = [
 class Model {
 	constructor() {
 		this.userArr = [];
+		this.arrOfQuestion = [];
+		self = this;
 	}
 	createNewUser(firstName, lastName, level) {
 		const newUser = new User(firstName, lastName);
 		newUser.setLevel(level);
 		this.userArr.push(newUser);
 	}
-	createArrOfQuestion(level) {
+	createArrOfQuestion() {
 		taskArr.forEach(element => {
-			if (level === element.level) {
-				const arrOfQuestion = [];
-				arrOfQuestion.push(element);
-				return arrOfQuestion;
+			if (this.userArr[0].getLevel() === element.level) {
+				this.arrOfQuestion.push(element);
 			}
 		});
+		this.arrOfQuestion = this.shuffle(this.arrOfQuestion);
+		console.log(this.arrOfQuestion);
 	}
-	checkAnswer(answer, question) {
-		if (answer === question.rightAnswer) {
+
+	checkAnswer(answer, numQuestion) {
+		if (answer == this.arrOfQuestion[numQuestion - 1].rightAnswer) {
 			console.log('Green Button');
 			console.log('Record number answer in person');
 		} else {
@@ -124,12 +136,113 @@ class Model {
 			console.log('Indicate right answer');
 		}
 	}
+	// Пока непонятно нужен ли этот метод!
+	// createQuestion(arrOfQuestion) {}
+	getQuestion(obj) {
+		return obj.question;
+	}
+	getAnswers(obj) {
+		return obj.answersArr;
+	}
+	getRightAnswer(obj) {
+		return obj.rightAnswer;
+	}
+
+	shuffle(arr) {
+		let j, temp;
+		for (var i = arr.length - 1; i > 0; i--) {
+			j = Math.floor(Math.random() * (i + 1));
+			temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+		}
+		return arr;
+	}
 }
 
 class Controller {
-	constructor() {}
+	constructor(model, view) {
+		this.model = model;
+		this.view = view;
+		this.amountQuestion = 0;
+		this.numberQuestion = 0;
+	}
+	start() {
+		let formBtn = document.querySelector('.form-login');
+		formBtn.addEventListener('click', e => {
+			e.preventDefault();
+			if (e.target.tagName === 'BUTTON' && this.numberQuestion === 0) {
+				let firstName = document.querySelector('#firstName').value;
+				let lastName = document.querySelector('#lastName').value;
+				let level = document.querySelector('#level').value;
+				this.model.createNewUser(firstName, lastName, level);
+				this.model.createArrOfQuestion();
+				this.getAmountQuestion();
+				this.renderQuestion(this.numberQuestion);
+			} else {
+				console.log('Ответь на вопрос, или нажми СЛЕДЮЩИЙ');
+			}
+			let questionBtn = document.querySelector('.form-test');
+			questionBtn.addEventListener('click', e => {
+				e.preventDefault();
+				if (e.target.classList.contains('answer-1')) {
+					this.model.checkAnswer(
+						e.target.textContent,
+						this.numberQuestion
+					);
+				} else if (e.target.classList.contains('answer-2')) {
+					this.model.checkAnswer(
+						e.target.textContent,
+						this.numberQuestion
+					);
+				} else if (e.target.classList.contains('answer-3')) {
+					this.model.checkAnswer(
+						e.target.textContent,
+						this.numberQuestion
+					);
+				} else if (e.target.classList.contains('answer-4')) {
+					this.model.checkAnswer(
+						e.target.textContent,
+						this.numberQuestion
+					);
+				} else if (e.target.classList.contains('next')) {
+					this.renderQuestion(this.numberQuestion);
+					console.log(
+						'Теперь ты нажимаешь на клавишу СЛЕДУЮЩИЙ ВОПРОС'
+					);
+				}
+			});
+		});
+	}
+	getAmountQuestion() {
+		this.amountQuestion = this.model.arrOfQuestion.length;
+	}
+	renderQuestion(num) {
+		if (num < this.amountQuestion) {
+			this.view.setQuestion(this.model.arrOfQuestion[num].question);
+			this.view.setAnswer(
+				this.model.shuffle(this.model.arrOfQuestion[num].answersArr)
+			);
+			this.numberQuestion++;
+			console.log(this.numberQuestion);
+		}
+	}
 }
 
 class View {
 	constructor() {}
+	setQuestion(question) {
+		let questionDiv = document.querySelector('.question');
+		questionDiv.textContent = question;
+	}
+	setAnswer(answers) {
+		answers.forEach((element, index) => {
+			let answerBtn = document.querySelector(`.answer-${index + 1}`);
+			answerBtn.textContent = element;
+		});
+	}
 }
+const view = new View();
+const model = new Model();
+const controller = new Controller(model, view);
+controller.start();
